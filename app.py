cd24bbb31d833798323cdd53563d2ca0175c8a8c
Fauzan Abdillah <fauzanebd@gmail.com>
2023-07-24 20:49:02 +0700

    initial commit

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def _train_new_data_sgd():
 
 def start_app():
     load_dotenv(".env")
-    run_sgd_background()
-    find_k()
+    # run_sgd_background()
+    # find_k()
     app.run()
 
 if __name__ == '__main__':
